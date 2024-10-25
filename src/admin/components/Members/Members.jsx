a46595b727562390a7e7c03c419865ab3df46f34
bhaskarAntony <rooparoopa7105@gmail.com>
2024-10-25 18:59:57 +0530

    all files

--- src/admin/components/Members/Members.jsx
+++ src/admin/components/Members/Members.jsx
@@ -13,7 +13,7 @@ const Members = () => {
     }, []);
     const fetchUsers = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/admin/get-user-data');
+            const response = await axios.get('http://localhost:5000/api/auth/users');
             setUsers(response.data);
             console.log(response.data)
         } catch (error) {
